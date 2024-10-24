6ba30dad98ed3210e6624c2c6865fe19482d4ef9
kduytran443 <trankhanhduy18@gmail.com>
2024-10-25 02:43:20 +0700

    #fp: find max min

--- src/fp/fp02/FP02Functional.java
+++ src/fp/fp02/FP02Functional.java
@@ -9,6 +9,19 @@ public class FP02Functional {
 
         int sum = sumOfList(numbers);
         System.out.println(sum);
+
+        int maxValue = findMax(numbers);
+        System.out.println(maxValue);
+        int minValue = findMin(numbers);
+        System.out.println(minValue);
+    }
+
+    private static int findMin(List<Integer> numbers) {
+        return numbers.stream().reduce(Integer.MAX_VALUE, (a, b) -> a <= b ? a : b);
+    }
+
+    private static int findMax(List<Integer> numbers) {
+        return numbers.stream().reduce(Integer.MIN_VALUE, (a, b) -> a >= b ? a : b);
     }
 
     private static int sumOfList(List<Integer> numbers) {
