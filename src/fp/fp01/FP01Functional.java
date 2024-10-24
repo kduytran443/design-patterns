86add3bb64b9c0c3d822a1a8b5e4939254d6fddf
kduytran443 <trankhanhduy18@gmail.com>
2024-10-25 02:20:57 +0700

    #fp: filter

--- src/fp/fp01/FP01Functional.java
+++ src/fp/fp01/FP01Functional.java
@@ -5,15 +5,25 @@ import java.util.List;
 public class FP01Functional {
 
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 10, 5, 2));
+        printEvenNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 10, 5, 2));
     }
 
     private static void print(int num) {
         System.out.println(String.format("Number: %s", num));
     }
 
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
     private static void printAllNumbersInListFunctional(List<Integer> integers) {
         integers.forEach(FP01Functional::print);
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> integers) {
+        integers.stream()
+                .filter(FP01Functional::isEven)
+                .forEach(FP01Functional::print);
     }
 
 }
--- src/fp/fp01/FP01Structured.java
+++ src/fp/fp01/FP01Structured.java
@@ -5,12 +5,24 @@ import java.util.List;
 public class FP01Structured {
 
     public static void main(String[] args) {
-        printAllNumbersInListStructured(List.of(12, 9, 13, 4, 5, 2));
+        printEvenNumbersInListStructured(List.of(12, 9, 13, 4, 6, 10, 5, 2));
+    }
+
+    private static void print(int num) {
+        System.out.println(String.format("Number: %s", num));
     }
 
     private static void printAllNumbersInListStructured(List<Integer> integers) {
         for (Integer i : integers) {
-            System.out.println(i);
+            print(i);
+        }
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> integers) {
+        for (Integer i : integers) {
+            if (i % 2 == 0) {
+                print(i);
+            }
         }
     }
 
